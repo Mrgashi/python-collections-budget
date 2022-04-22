bdf04cbf084772756009ecc4c52bf8b259f19586
Mirdon Gashi <mirdongashi@hl7tjq6t6x.home>
2022-04-22 13:10:26 +0200

    stuck on timeit.timeit

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,4 +1,5 @@
 from . import Expense
+import timeit
 import matplotlib.pyplot as plt
 
 def main():
@@ -7,12 +8,20 @@ def main():
     divided_for_loop = expenses.categorize_for_loop()
     divided_set_comp = expenses.categorize_set_comprehension()
     
-    if divided_set_comp == divided_for_loop:
+    if divided_set_comp != divided_for_loop:
         print("Sets are NOT equal by == test")
 
     for a, b in zip(divided_for_loop, divided_set_comp):
-        if (not a.issubset(b) and b.issubset(a)):
+        if (not a.issubset(b) and not b.issubset(a)):
             print("Sets are NOT equal by subset test")
+
+    timeit.timeit(stmt="expenses.categorize_for_loop()", setup="""from . import Expense
+            expenses = Expense.Expenses()
+            expenses.read_expenses('data/spending_data.csv')
+            """, number=100000, globals=globals())
+
 
 if __name__ == "__main__":
     main()
+
+
